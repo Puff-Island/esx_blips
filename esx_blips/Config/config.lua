122f4b047f0fbe307bd6d05a9fda8cb62c95af3e
Toño <modz.gta@live.com>
2022-11-09 23:23:30 -0400

    Delete esx_blips directory

--- esx_blips/Config/config.lua
+++
@@ -1,4 +0,0 @@
-Config = {
-  OpenContext = 'createblip', -- Open Context Menu to create Blip.
-  DeleteBlip = 'deleteblip' -- To use you must be in the location of the blip. Afther using the command it becomes prety self explanatory.
-}
